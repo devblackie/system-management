9c587f85ba43aab0fa53c51d42518f51473f6d4e
Newton <newtieblackie@gmail>
2022-09-10 20:41:13 +0300

    Import Growers , Litter and Sow componens to Records

---
+++ src/components/Records.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Growers, Litter, Sow} from "../components";
+
+
+
+
+
+
+export default function records() {
+ 
+
+  return (
+    <div>
+        
+    </div>
+  );
+
+ 
+
+}
