0997ec2d7eefe1ad7dff9759ff7b8aef05c9b4e6
Newton <newtieblackie@gmail>
2022-09-10 15:25:31 +0300

    Added style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import styles from './styles';
 
 export default function App() {
   return (
-    <div>App</div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX}`}></div>
+    </div>
   )
 }
