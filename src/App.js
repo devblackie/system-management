ef0a88b0e579d0f20cdbf64402c1da71779d5648
Newton <newtieblackie@gmail>
2022-09-10 15:36:18 +0300

    Added componets

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import styles from './styles';
 export default function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <div className={`${styles.paddingX}`}></div>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+      
     </div>
   )
 }
