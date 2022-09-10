a6c555b90183401f411ff6f7275aafc72da5375c
Newton <newtieblackie@gmail>
2022-09-10 22:51:05 +0300

    Deleted growers, litter and sow component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <div>
+        <a href="/#">
+        <NavLink className="nav-link " to='/records'>Records</NavLink>
+        {/* <NavLink className="nav-link " to='/sales'>Sales</NavLink>
+        <NavLink className="nav-link " to='/expenditure'>Expenditure</NavLink>
+        <NavLink className="nav-link " to='/feeding'>Feeding</NavLink> */}
+        </a>
+    </div>
+  );
+}
