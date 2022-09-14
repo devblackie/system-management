3f65f655b3c03ac91cf4a5a49087ccdb4bd6bdcc
Newton <newtieblackie@gmail>
2022-09-14 10:57:28 +0300

    Updated the course pages

---
+++ src/CourseApplication.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import './CourseApplication.css';
+import { useState } from 'react';
+
+function CourseApplication({handlePosting}) {
+  const [schoolData, setSchoolData] = useState({
+    course_name: '',
+    fees: '',
+    department: '',
+    course_duration: '',
+  })
+
+  function handleSubmit(e){
+    e.preventDefault();
+    fetch(`https://star-light-uni.herokuapp.com/courses`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(schoolData)
+    })
+    .then(r => r.json())
+    .then(data => {
+        handlePosting(data)
+    })
+
+    setSchoolData({
+        course_name: '',
+        fees: '',
+        department: '',
+        course_duration: '',
+    })
+}
+
+  function handleChange(e){
+    setSchoolData({
+        ...schoolData, [e.target.name]: e.target.value,
+    });
+} 
+  return (
+    <div className="container3">
+      <div className="contact-box1">
+        <div className="lefti"></div>
+        <div className="right">
+          <h2>Enroll Now</h2>
+          <form>
+            <input
+              type='text'
+              name='course_name'
+              required
+              className ="field"
+              placeholder='Course Name'
+              value={schoolData.course_name}
+              onChange={handleChange}
+            ></input>
+            <br />
+            <input
+              type='number'
+              name='fees'
+              required
+              className='field'
+              placeholder='Fees'
+              value={schoolData.fees}
+              onChange={handleChange}
+            ></input>
+            <input
+               type='text'
+               name='department'
+               placeholder="Department"
+               className='field'
+               value={schoolData.department}
+              onChange={handleChange}
+            ></input>
+           <input
+               type='number'
+               name='course_duration'
+               required
+               className='field'
+               placeholder='Course Duration in Months'
+               value={schoolData.course_duration}
+              onChange={handleChange}
+           ></input>
+          <button className="btn2" onClick={handleSubmit}>Send</button>
+          </form>
+        </div>
+      </div>
+	  </div>
+  )
+}
+
+export default CourseApplication;
---
+++ src/CoursePage.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState, useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+import CourseApplication from './CourseApplication';
+import CourseDetails from "./CourseDetailsPage";
+
+function CoursePage(){
+    const [allCourses, setAllCourses] = useState([])
+    // const navigate = useNavigate()
+
+    useEffect(() => {
+      fetch('https://star-light-uni.herokuapp.com/courses')
+      .then(r => r.json())
+      .then(data => setAllCourses(data))
+    }, []);
+
+    function handlePosting(data){
+      setAllCourses([...allCourses, data])
+    }
+
+    function deleteCourse(id){
+      fetch(`https://star-light-uni.herokuapp.com/courses/${id}`,{
+          method: "DELETE",
+      })
+      .then(r => r.json())
+      .then(() => {
+          const goThru = allCourses.filter((course) => course.id !== id)
+              setAllCourses(goThru)
+          })
+    }
+
+    function handleUpdateCourse(updateCourse) {
+      const updatedCourse = allCourses.map((course) => {
+        if (course.id === updatedCourse.id) {
+          return updatedCourse;
+        } else {
+          return course;
+        }
+      });
+      setAllCourses(updatedCourse);
+    }
+
+    return (
+      <div>
+        <CourseApplication handlePosting={handlePosting}/>
+        <CourseDetails allCourses ={allCourses} deleteCourse={deleteCourse}/>
+      </div>
+  )
+}
+
+export default CoursePage;
