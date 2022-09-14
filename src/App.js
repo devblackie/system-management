82661acc2c7ef45239ce083956532c34dc2bc3ed
Newton <newtieblackie@gmail>
2022-09-14 10:49:34 +0300

    Routed the elements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Outlet} from 'react-router-dom' 
-import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import CoursePage from './CoursePage';
+import About from "./About"
+import ContactUs from "./ContactUs"
+import NavBar from './NavBar';
+import TeachersPage from './TeacherPage';
+import StudentPage from './StudentsPage';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Book_Review
-      </header>
-      <Outlet />
-      <Home />
-      <footer></footer>
-    </div>
+  
+      <div>
+      <NavBar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/courses' element={<CoursePage />} />
+        <Route exact path='/students' element={<StudentPage />} />
+        <Route exact path='/teachers' element={<TeachersPage />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/contactus' element={<ContactUs />} />
+      </Routes>
+      </div>
+
   );
 }
 
