e3bc4f5f923e7ccd5f1049ce13eea22055f9b281
Newton Black <newtieblackie@gmail.com>
2022-11-15 14:19:39 +0300

    Navbarflex

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,62 +5,44 @@ import { useState } from "react";
 export default function NavBar() {
   const [navbar, setNavbar] = useState(false);
   return (
-    <nav className="w-full bg-white shadow">
-      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-        <div>
-          <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <h1 className="">BrightGam College</h1>
-          </div>
-        </div>
-        <div>
-          <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
-            <li className="text-gray-600 hover:text-blue-600">
-              <NavLink className="hover:border-b-4 pb-2 border-white" to="/">
-                HOME
-              </NavLink>
-            </li>
-            <li className="text-gray-600 hover:text-blue-600">
-              <NavLink
-                className="hover:border-b-4 pb-2 border-white"
-                to="/courses"
-              >
-                COURSES
-              </NavLink>
-            </li>
-            <li className="text-gray-600 hover:text-blue-600">
-              <NavLink
-                className="hover:border-b-4 pb-2 border-white"
-                to="/students"
-              >
-                STUDENTS
-              </NavLink>
-            </li>
-            <li className="text-gray-600 hover:text-blue-600">
-              <NavLink
-                className="hover:border-b-4 pb-2 border-white"
-                to="/teachers"
-              >
-                TEACHERS
-              </NavLink>
-            </li>
-            <li className="text-gray-600 hover:text-blue-600">
-              <NavLink
-                className="hover:border-b-4 pb-2 border-white"
-                to="/about"
-              >
-                ABOUT
-              </NavLink>
-            </li>
-            <li className="text-gray-600 hover:text-blue-600">
-              <NavLink
-                className="hover:border-b-4 pb-2 border-white"
-                to="/contactus"
-              >
-                CONTACT US
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+    <nav className="">
+      <div className="logoname">
+        <h1 className="">BrightGam College</h1>
+      </div>
+
+      <div>
+        <ul className="">
+          <li className="linkage">
+            <NavLink className="" to="/">
+              HOME
+            </NavLink>
+          </li>
+          <li className="linkage">
+            <NavLink className="" to="/courses">
+              COURSES
+            </NavLink>
+          </li>
+          <li className="linkage">
+            <NavLink className="" to="/students">
+              STUDENTS
+            </NavLink>
+          </li>
+          <li className="linkage">
+            <NavLink className="" to="/teachers">
+              TEACHERS
+            </NavLink>
+          </li>
+          <li className="linkage">
+            <NavLink className="" to="/about">
+              ABOUT
+            </NavLink>
+          </li>
+          <li className="linkage">
+            <NavLink className="" to="/contactus">
+              CONTACT US
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
